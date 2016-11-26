f57762cbbb034461ff0f8339fe733ccb00812a93
daniyalakhan <daniyalakhan@programmer.net>
2016-11-27 04:45:03 +0500

    Practice Set 2 - Lecture 2, Added a gists to the (files: MainActivity.java & activity_main.xml), 27Nov16

--- PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.practiceset2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,23 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calculate();
+        // PASTE CODE YOU WANT TO TEST HERE
+
     }
 
-    public void calculate(){
-        int day1 = 15;
-        int day2 = 22;
-        int day3 = 18;
-        display((day1 + day2 + day3) / 3);
-    }
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
 
-//    public void display (int i) {
-//        TextView t = (TextView) findViewById(R.id.display_text_view);
-//        t.setText(""+i);
-//    }
-
-    public void display (float i) {
+    public void display(String text) {
         TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(""+i);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
     }
 }
